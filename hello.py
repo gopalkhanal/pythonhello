db0a28db6b526624995db7e9f236a50fd7d4424c
gopalkhanal <smegopal@hotmail.com>
2017-10-27 11:44:32 -0700

    second commit

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 import random
 n = random.randint(1, 99)
 guess = int(input("Enter an integer from 1 to 99: "))
+print ("this is the second commit")
 while n != "guess":
     print
     if guess < n:
